05d8052fa8ac04bfd735467d047a6e99ee0d6cd0
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2015-01-12 23:33:41 -0500

    Initial Commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by Don Wettasinghe on 1/12/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
