6b6f305d9e0a94c3c065b554c63b3753da2870ed
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2015-01-14 08:29:52 -0500

    Added re-order list

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -11,6 +11,9 @@
 @interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *contentText;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property BOOL editBtnPress;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *onAddBtn;
 
 @property NSMutableArray *dataArray;
 
@@ -20,16 +23,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.dataArray = [NSMutableArray arrayWithObjects:self.contentText.text, nil];
+    self.editBtnPress=false;
 }
-- (IBAction)onAddButtonPressed:(UIButton *)sender {
+
+- (IBAction)onSwipeGestureColorChange:(UISwipeGestureRecognizer*)sender {
     
+    
+}
+- (IBAction)onAddButtonPressed:(UIBarButtonItem *)sender {
     [self.dataArray addObject:self.contentText.text];
     
     [self.tableView reloadData];
+    self.contentText.text=@"";
+    [self.view endEditing:YES];
+    
+}
+
+//- (IBAction)onAddButtonPressed:(UIButton *)sender {
+//    
+//    [self.dataArray addObject:self.contentText.text];
+//    
+//    [self.tableView reloadData];
+//    self.contentText.text=@"";
+//    [self.view endEditing:YES];
+//}
+
+
+- (IBAction)onEditButtonPressed:(UIBarButtonItem*)sender {
+    
+    if (self.editBtnPress==false) {
+        self.editBtnPress=true;
+       [self.tableView setEditing:true ];
+       sender.title=@"Done";
+    }else{
+        self.editBtnPress=false;
+       [self.tableView setEditing:false];
+       sender.title=@"Edit";
+    }
+    
 }
 
 #pragma mark UItableView Delegates
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
@@ -40,10 +85,30 @@
     
     UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"Cell"];
     cell.textLabel.text=[NSString stringWithFormat:@"%@", [self.dataArray objectAtIndex:indexPath.row] ];
-   // [tableView insertRowsAtIndexPaths:indexPath.row withRowAnimation:UITableViewRowActionStyleNormal];
+    cell.textLabel.highlightedTextColor = [UIColor greenColor];
     
     return cell;
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    
+}
+
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    NSString *arrayItem=[self.dataArray objectAtIndex:sourceIndexPath.row];
+    [self.dataArray removeObject:arrayItem];
+    [self.dataArray insertObject:arrayItem atIndex:destinationIndexPath.row];
+    
+    [self.tableView reloadData];
+    
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    [self.dataArray removeObjectAtIndex:indexPath.row];
+    [self.tableView reloadData];
+}
 
 @end
